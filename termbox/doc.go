e1dd74bed69a830a754d6aa045ebc1f0f1ddd99c
jimt <jimteeuwen@gmail.com>
2012-10-01 01:07:01 +0200

    Implements keyboard backend for termbox-go.

---
+++ termbox/doc.go
@@ -0,0 +1,8 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+/*
+This is a keyboard backend for termbox.
+Termbox should be initialized by the host application.
+*/
+package termbox
